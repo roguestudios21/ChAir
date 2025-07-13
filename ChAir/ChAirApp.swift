84e1ddac53bbe26e33639ed565a9a190acd71d32
roguestudios21 <atharv.choughule@gmail.com>
2025-07-13 13:27:22 +0530

    Initial Commit

---
+++ ChAir/ChAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChAirApp.swift
+//  ChAir
+//
+//  Created by Atharv  on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChAir/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChAir
+//
+//  Created by Atharv  on 13/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
